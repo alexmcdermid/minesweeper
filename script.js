c6f8586589852edf20783739c7b7fc1f989a2c00
alexmcdermid <alexander.mcdermid@gmail.com>
2021-09-09 10:05:14 -0700

    basic 10x10 board with bombs

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 //constants
 const grid = document.querySelector('.grid');
-const choose = document.querySelector('.choose');
+const difficulty = document.querySelector('.difficulty');
 const buttonEasy = document.querySelector('.easy');
 const buttonHard = document.querySelector('.hard');
-const width = 20;
-const height = 20;
-const boardSize = width*height;
+const size = document.querySelector('.size');
+const buttonSmall = document.querySelector('.small');
+const buttonLarge = document.querySelector('.large');
 //variables
+var isGameOver = false;
 let bombs = 0;
 let squares = [];
+var width = 10;
+var height = 10;
+var boardSize = width*height;
+
 //event listeners
 document.querySelector('.easy').addEventListener('click', function() {
     bombs = 10;
     buttonEasy.parentNode.removeChild(buttonEasy);
     buttonHard.parentNode.removeChild(buttonHard);
-    choose.parentNode.removeChild(choose);
+    difficulty.parentNode.removeChild(difficulty);
     createBoard();
 });
 
@@ -22,9 +27,30 @@ document.querySelector('.hard').addEventListener('click', function() {
     bombs = 20;
     buttonEasy.parentNode.removeChild(buttonEasy);
     buttonHard.parentNode.removeChild(buttonHard);
-    choose.parentNode.removeChild(choose);
+    difficulty.parentNode.removeChild(difficulty);
     createBoard();
 });
+
+//was messing with the idea of being able to change the board size. kind of a pain
+// document.querySelector('.small').addEventListener('click', function() {
+//     buttonEasy.parentNode.removeChild(buttonSmall);
+//     buttonHard.parentNode.removeChild(buttonLarge);
+//     difficulty.parentNode.removeChild(size);
+//     renderBoard();
+// });
+
+// document.querySelector('.large').addEventListener('click', function() {
+//     buttonEasy.parentNode.removeChild(buttonSmall);
+//     buttonHard.parentNode.removeChild(buttonLarge);
+//     difficulty.parentNode.removeChild(size);
+//     renderBoard();
+// });
+
+// function renderBoard() {
+//     grid.style.height = height*height;
+//     grid.style.width = width*width;
+// }
+
 //functions
 function createBoard() {
     const bombsArray = Array(bombs).fill('bomb');
@@ -34,11 +60,12 @@ function createBoard() {
 
     for(let i = 0; i<boardSize; i++) {
         const square = document.createElement('div');
+        square.style.width = width;
+        square.style.height = height;
         square.setAttribute('id', i);
         square.classList.add(gameArray[i]);
         grid.appendChild(square);
-        squares.push(square);
+        squares.push(square); 
     }
-
+    
 }
-
